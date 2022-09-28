59e7bccebdfb914a35721bcf498321929c84fd8a
sanskar sahu <sahup3296@gmail.com>
2022-09-28 11:45:33 +0000

    Update 13 files

---
+++ src/theme.tsx
@@ -0,0 +1,19 @@
+import { Theme } from "theme-ui";
+
+export const theme: Theme = {
+  colors: {
+    header: ["#B2C6EC", "#D9E3F5", "#658ED9"],
+    footer: "#003049",
+    background: "#F2F6FC",
+    primary: "#313D5A",
+    secondary: "#73628A",
+    alert: "#d62828",
+    warning: "#fcbf49",
+    button: "#f77f00",
+  },
+  links:{
+      text:{
+          textDecoration:"none",
+      }
+  }
+};
